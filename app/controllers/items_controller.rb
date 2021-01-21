7cf41cee909c5c770862a8aba7035e9a3368139b
敦史 <midori06180410@gmil.com>
2021-01-21 18:11:21 +0900

    itemsファイルにカラムの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
-  
+
+ def edit
+  @item = Item.new
+  unless user.signed_in?
+    root_to new_user_session
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2021_01_21_051639) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_info", null: false
+    t.string "item_category", null: false
+    t.string "item_sales_status", null: false
+    t.string "item_shipping_fee_status", null: false
+    t.string "item_prefecture", null: false
+    t.string "item_scheduled_delivery", null: false
+    t.string "item_price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
