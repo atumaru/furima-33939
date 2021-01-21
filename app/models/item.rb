7393b633912e6850b4c0f5f51e943f2ea0c5669f
敦史 <midori06180410@gmil.com>
2021-01-21 18:53:01 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  validates :item_name,presence: true
+  validates :item_info,presence: true
+  validates :item_category,presence: true
+  validates :item_sales_status,presence: true
+  validates :item_shipping_fee_status,presence: true
+  validates :item_prefecture,presence: true
+  validates :item_scheduled_delivery,presence: true
+  validates :item_price,presence: true
+  validates :item_price,numericality:{reater_than_or_equal_to:300,less_than_or_qruel_to:9999999}
+  validates :item_price,format:{/\A[0-9]+\z/}
 end
