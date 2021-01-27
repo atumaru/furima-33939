79d32024d3393be228f44524e70722e5f231b5b7
敦史 <midori06180410@gmil.com>
2021-01-27 14:44:00 +0900

    items controller beforeアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only:[:show,:edit,:update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,15 +22,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+   
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -38,6 +39,10 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def move_to_index
     @item = Item.find(params[:id])
@@ -48,3 +53,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
+
+
