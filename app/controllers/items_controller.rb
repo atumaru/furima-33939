989e9ea300333a1c15a6f9468d53ebd62d1ff925
敦史 <midori06180410@gmil.com>
2021-01-27 14:52:36 +0900

    不要な改行の削除　before_action追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create edit]
+  before_action :authenticate_user!, only: %i[new create edit update]
   before_action :move_to_index, only: [:edit]
-  before_action :set_item, only:[:show,:edit,:update,:move_to_index]
+  before_action :set_item, only: %i[show edit update move_to_index]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,16 +21,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  
-  end
+  def show; end
 
-  def edit
-    
-  end
+  def edit; end
 
   def update
-   
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,7 +40,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    
     redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
   end
 
@@ -53,5 +47,3 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
 end
-
-
