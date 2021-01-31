5b0592953f178a83a292504c54d62102411b935a
敦史 <midori06180410@gmil.com>
2021-01-31 11:29:22 +0900

    カンマ削除

--- db/migrate/20210128021249_create_deliveries.rb
+++ db/migrate/20210128021249_create_deliveries.rb
@@ -5,7 +5,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id,                  null: false
       t.string :city,           default: "",     null: false
       t.string :address,   default: "",          null: false
-      t.string :building,  default: "",          
+      t.string :building,  default: ""          
       t.string :phone_number,                    null: false
       t.references :order,                     foreign_key: true
 
